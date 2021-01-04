3ce5135d7d44b933f662b57698500f6989ad24cb
malika tahri <malika.tahri23@gmail.com>
2021-01-04 09:06:29 +0100

    waaw

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,37 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const path = require('path')
-const productRouter = require('./routers/productRouter.js')
-const userRouter = require('./routers/userRouter.js')
-const orderRouter = require('./routers/orderRouter.js')
-const uploadRouter = require('./routers/uploadRouter.js')
+  import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import config from './config';
+import userRoute from './routes/userRoute';
+import productRoute from './routes/productRoute';
+import orderRoute from './routes/orderRoute';
+import uploadRoute from './routes/uploadRoute';
 
-dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/hloutounsi', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-app.use('/api/uploads', uploadRouter);
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-app.use('/api/orders', orderRouter);
+app.use(bodyParser.json());
+app.use('/api/uploads', uploadRoute);
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/orders', orderRoute);
 app.get('/api/config/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+  res.send(config.PAYPAL_CLIENT_ID);
 });
-app.get('/api/config/google', (req, res) => {
-  res.send(process.env.GOOGLE_API_KEY || '');
-});
-app.use('/uploads', express.static(path.join(path.resolve(), '/uploads')));
-app.use(express.static(path.join(path.resolve(), '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(path.resolve(), '/frontend/build/index.html'))
-);
-// app.get('/', (req, res) => {
-//   res.send('Server is ready');
-// });
-
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
-const port = process.env.PORT || 5001;
-app.listen(port, () => {
-  console.log(`Serve at http://localhost:${port}`);
+app.listen(config.PORT, () => {
+  console.log('Server started at http://localhost:5000');
 });
