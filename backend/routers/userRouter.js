d788cd796a5a6b514e59e1c2cd006d2df6462f94
malika tahri <malika.tahri23@gmail.com>
2021-01-04 08:58:36 +0100

    chayhitli riki

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const expressAsyncHandler = require('express-async-handler')
 const data = require('../data.js')
 const User = require('../models/userModel.js')
