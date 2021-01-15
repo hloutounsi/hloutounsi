5931259f74202e52789e0a8def8ca5f2e97bf009
malika tahri <malika.tahri23@gmail.com>
2021-01-15 23:52:10 +0100

    OK

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,10 @@ app.get('/api/config/paypal', (req, res) => {
 app.get('/api/config/google', (req, res) => {
   res.send(config.GOOGLE_API_KEY);
 });
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 );
 
 app.post('/api/send', async (req, res) => {
